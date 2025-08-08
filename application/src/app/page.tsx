e3927f06df5977fd4dc35b3be2c0457781ca58c3
jenishadh <202624708+jenishadh@users.noreply.github.com>
2025-08-08 16:33:53 +0000

    chore: initialize project

---
+++ application/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="p-5 space-y-1">
+      <h1>Music Recognition System</h1>
+      <Button>Hello World</Button>
+    </main>
+  );
+}
